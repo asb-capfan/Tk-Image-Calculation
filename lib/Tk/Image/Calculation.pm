7b86504661ce19156e802851a93071b040b788e5
Alex <asb@cpan.org>
2021-03-12 20:00:19 +0100

    increase version

--- lib/Tk/Image/Calculation.pm
+++ lib/Tk/Image/Calculation.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 #-------------------------------------------------
-$Tk::Image::Calculation::VERSION = '0.04';
+$Tk::Image::Calculation::VERSION = '0.05';
 #-------------------------------------------------
 
 sub new
